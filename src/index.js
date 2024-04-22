7f6bb235540035efe138eb91cb71395ac05c05a9
Fatimah <fatimah.zulfiqar@cern.ch>
2024-04-22 10:28:25 +0200

    config: updated mirador for configurable settings

--- src/index.js
+++ src/index.js
@@ -4,67 +4,74 @@ const params = new URLSearchParams(location.search)
 
 var manifest = params.get('m')
 var canvas = params.get('c')
+var config = {}
+var annotations = {}
 
 if (manifest && canvas) {
   console.log ('detected URL params')
 } else {
   console.log ('detected InvenioRDM preview URL')
-
-  //compute manifest URL from InvenioRDM preview URL
-  manifest = location.href.split('?')[0]
-  manifest=manifest.replace('records/','api/iiif/record:');
-  manifest=manifest.replace(/preview.+$/, 'manifest');
-
-  canvas = location.href.split('?')[0]
-  canvas=canvas.replace('records/','api/iiif/record:');
-  canvas=canvas.replace('/preview/', '/canvas/');
-
-  if (params.get('preview')) {
-    manifest = manifest.replace('record:','draft:');
-    canvas = canvas.replace('record:','draft:');
-  }
-
+  manifest = document.getElementById('m3-dist').getAttribute('data-manifest');
+  canvas = document.getElementById('m3-dist').getAttribute('data-canvas');
+  config = JSON.parse(document.getElementById('m3-dist').getAttribute('data-config'));
+  annotations = JSON.parse(document.getElementById('m3-dist').getAttribute('data-annotations'));
 }
 
   console.log(manifest)
   console.log(canvas)
 
-const config = {
-  id: 'm3-dist',
-  workspaceControlPanel: {
-    enabled: false,
-  },
-  window: {
-    allowClose: false,
-    allowFullscreen: true,
-    allowTopMenuButton: true,
-    allowMaximize: false,
-    allowWindowSideBar: false,
-    defaultSidebarPanelHeight: 201,
-    defaultSidebarPanelWidth: 235,
-    defaultView: 'xsingle',
-    hideWindowTitle: true,
-    sideBarOpen: false,
-    switchCanvasOnSearch: false,
-    panels: {
-      info: true,
-      attribution: false,
-      canvas: false,
-      annotations: false,
-      search: false,
-      layers: false,
+function initMirador({ id, manifest, canvas, config, annotations }) {
+  const defaultConfig = {
+    workspaceControlPanel: {
+      enabled: false,
     },
-    views: [
-      { key: 'single', behaviors: ['individuals'] },
-      { key: 'book', behaviors: ['paged'] },
-      { key: 'scroll', behaviors: ['continuous'] },
-      { key: 'gallery' },
-    ],
-  },
-  windows: [{
-    manifestId: manifest,
-    canvasId: canvas,
-  }],
-};
+    window: {
+      allowClose: false,
+      allowFullscreen: true,
+      allowTopMenuButton: true,
+      allowMaximize: false,
+      allowWindowSideBar: true,
+      defaultSidebarPanelHeight: 201,
+      defaultSidebarPanelWidth: 235,
+      defaultView: 'xsingle',
+      hideWindowTitle: true,
+      sideBarOpen: false,
+      switchCanvasOnSearch: false,
+      panels: {
+        info: true,
+        attribution: false,
+        canvas: false,
+        annotations: false,
+        search: false,
+        layers: false,
+      },
+      views: [
+        { key: 'single', behaviors: ['individuals'] },
+        { key: 'book', behaviors: ['paged'] },
+        { key: 'scroll', behaviors: ['continuous'] },
+        { key: 'gallery' },
+      ],
+    },
+  };
 
-Mirador.viewer(config);
+  const mergedConfig = {
+    id: id,
+    windows: [{
+      manifestId: manifest,
+      canvasId: canvas,
+      thumbnailNavigationPosition: "far-bottom",
+    }],
+    ...defaultConfig,
+    ...(config || {}),
+  };
+  console.log(mergedConfig);
+  Mirador.viewer(mergedConfig);
+}
+
+initMirador({
+  id: 'm3-dist',
+  manifest: manifest,
+  canvas: canvas,
+  config: config,
+  annotations: annotations
+});
